a8f1ba0b1f45c7a011cc6b81bac8bb3f3be82f63
Daniele Santos <js_dani@hotmail.com>
2020-08-28 17:08:30 -0300

    Change API mocky baseURL

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_MOCKY = axios.create({
-  baseURL: "http://www.mocky.io/v2/5a25fade2e0000213aa90776",
+  baseURL: "http://www.mocky.io/v2",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
