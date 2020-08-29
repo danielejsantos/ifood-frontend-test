3552b217bd176f8561a89ad999a93e66a8a2ce00
Daniele Santos <js_dani@hotmail.com>
2020-08-29 15:16:57 -0300

    Remove unused headers attributes

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -1,17 +1,13 @@
 import axios from "axios";
+import { getItem } from "../services/storage";
 
 export const API_MOCKY = axios.create({
   baseURL: "http://www.mocky.io/v2",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
 });
 
 export const API_SPOTIFY = axios.create({
-  baseURL: "https://api.spotify.com/v1/browse/featured-playlists",
+  baseURL: "https://api.spotify.com/v1",
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    Authorization: `Bearer ${getItem()}`,
   },
 });
