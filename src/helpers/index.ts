7697051a949ad5b3d8bcccdfe7822f5a28038620
Daniele Santos <js_dani@hotmail.com>
2020-08-29 15:10:08 -0300

    Add seconds to timestamp function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,4 +3,4 @@ import { format } from "date-fns";
 export const mapValues = (index: number, items: any) =>
   items?.filters[index].values.map((item: any) => item);
 
-export const timestamp = format(new Date(), "yyyy-MM-dd'T'HH:mm");
+export const timestamp = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss");
